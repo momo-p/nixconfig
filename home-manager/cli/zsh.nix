cb79af9ad08d92478b1e3cac8375adecbbafb665
momo-p <momo-p@chong-arisu.id.vn>
2025-03-06 15:58:30 +0700

    fix: turn off native zsh `autocomplete` for latency

--- home-manager/cli/zsh.nix
+++ home-manager/cli/zsh.nix
@@ -1,10 +1,11 @@
-{pkgs, ...}: {
+{
   programs.zsh = {
     enable = true;
     history = {
       size = 2000;
       save = 10000;
     };
+    enableCompletion = false;
     antidote = {
       enable = true;
       plugins = [

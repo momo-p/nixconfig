e36c61613a06f7502f7b5e98a0f45820868d9f13
momo-p <momo-p@chong-arisu.id.vn>
2025-05-30 20:17:29 +0700

    build: upgrade to `25.05` nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=25.05";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     stylix.url = "github:danth/stylix";

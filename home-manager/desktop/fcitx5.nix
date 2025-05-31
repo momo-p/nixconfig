4a2de49c09d68c625c0f668e126ec598556d069b
momo-p <momo-p@chong-arisu.id.vn>
2025-05-31 15:03:55 +0700

    feat: add default `fcitx5` IME config

--- home-manager/desktop/fcitx5.nix
+++ home-manager/desktop/fcitx5.nix
@@ -2,6 +2,24 @@
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
-    fcitx5.addons = with pkgs; [fcitx5-bamboo fcitx5-anthy];
+    fcitx5 = {
+      addons = with pkgs; [
+        fcitx5-bamboo
+        fcitx5-anthy
+      ];
+      settings = {
+        inputMethod = {
+          GroupOrder."0" = "Default";
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "us";
+            DefaultIM = "anthy";
+          };
+          "Groups/0/Items/0".Name = "keyboard-us";
+          "Groups/0/Items/1".Name = "anthy";
+          "Groups/0/Items/2".Name = "bamboo";
+        };
+      };
+    };
   };
 }

e9801e30d2b591a8b1a147c6354a61ab19978ebf
momo-p <momo-p@kafuu-chino.cafe>
2026-03-21 18:11:17 +0700

    feat: add wine and remove override for heroic launcher

--- modules/gaming/default.nix
+++ modules/gaming/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
+    ./wine.nix
     ./steam.nix
     ./gamescope.nix
     ./minecraft.nix
--- modules/gaming/steam.nix
+++ modules/gaming/steam.nix
@@ -2,6 +2,7 @@
   programs.steam = {
     enable = true;
     gamescopeSession.enable = true;
+    protontricks.enable = true;
     extraCompatPackages = with pkgs; [
       proton-ge-bin
     ];

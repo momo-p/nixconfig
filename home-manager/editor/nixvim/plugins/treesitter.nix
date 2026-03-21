08c2c91ceac4497401c2f040024963fc333109b3
momo-p <momo-p@kafuu-chino.cafe>
2026-03-21 18:09:20 +0700

    chore: update nixvim new config structure

--- home-manager/editor/nixvim/plugins/treesitter.nix
+++ home-manager/editor/nixvim/plugins/treesitter.nix
@@ -2,26 +2,25 @@
   programs.nixvim.plugins = {
     treesitter = {
       enable = true;
-      settings = {
-        auto_install = true;
-        highlight = {
-          additional_vim_regex_highlighting = true;
-          custom_captures = {};
-          enable = true;
-        };
 
-        incremental_selection = {
-          enable = true;
-          keymaps = {
-            init_selection = "gnn";
-            node_decremental = "grm";
-            node_incremental = "grn";
-            scope_incremental = "grc";
-          };
+      auto_install = true;
+      highlight = {
+        additional_vim_regex_highlighting = true;
+        custom_captures = {};
+        enable = true;
+      };
+
+      incremental_selection = {
+        enable = true;
+        keymaps = {
+          init_selection = "gnn";
+          node_decremental = "grm";
+          node_incremental = "grn";
+          scope_incremental = "grc";
         };
-        indent = {
-          enable = true;
-        };
+      };
+      indent = {
+        enable = true;
       };
     };
   };
